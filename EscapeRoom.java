951c77dd7b226f4dfcff29d09788eab1ec802d94
kaushik2008 <85648543+kaushik2008@users.noreply.github.com>
2024-09-20 14:17:17 -0700

    Added lots of features

--- EscapeRoom.java
+++ EscapeRoom.java
@@ -65,30 +65,74 @@ public class EscapeRoom
     boolean play = true;
     while (play)
     {
-      game.pickupPrize();
       System.out.print("Enter your movement: ");
       String movement = UserInput.getValidInput(validCommands);
       if (movement.equals("right") || movement.equals("r")) { 
         game.movePlayer(60, 0);
+        score++; 
       } 
       if (movement.equals("left") || movement.equals("l")) { 
         game.movePlayer(-60, 0);
+        score++; 
       }
       if (movement.equals("up") || movement.equals("u")) { 
         game.movePlayer(0, -60);
+        score++; 
       }
       if (movement.equals("down") || movement.equals("d")) { 
           game.movePlayer(0, 60);
+          score++; 
       }
       if (movement.equals("quit") || movement.equals("q")) {
+        //game.endGame();
         play = false;
       }
-    }
+      if (movement.equals("pickup") || movement.equals("p")) { 
+        game.pickupPrize();
+        score++; 
+      }
+      if (movement.equals("replay")) { 
+        game.replay();
+      }
+      if (movement.equals("jumpup") || movement.equals("ju")) {
+        game.movePlayer(0, -120);
+        score++; 
+      }
+      if (movement.equals("jumpright") || movement.equals("jr")) { 
+        game.movePlayer(120, 0);
+        score++; 
+      }
+      if (movement.equals("jumpleft") || movement.equals("jl")) {
+        game.movePlayer(-120, 0);
+        score++; 
+      }
+      if (movement.equals("jumpdown") || movement.equals("jd")) { 
+        game.movePlayer(0, 120);
+        score++; 
+      }
+      if  (movement.equals("help") || movement.equals("?")) { 
+        System.out.println("To use these functions you must type the command in the terminal");
+        System.out.println("right or r: move the player 1 square right");
+        System.out.println("left or l: move the player 1 square left");
+        System.out.println("up or u: move the player 1 square up");
+        System.out.println("down or d: move the player 1 square down");
+        System.out.println("quit or q: quit the game");
+        System.out.println("replay: restart the game");
+        System.out.println("pickup or p: pickup the prize");
+        System.out.println("jumpup or ju: jumps the player 2 squares up");
+        System.out.println("jumpdown or jd: jump the player 2 squares down");
+        System.out.println("jumpright or jr: jump the player 2 squares right");
+        System.out.println("jumpleft or lf: jump the player 2 squares left");
+        System.out.println("help or ?: get a list of valid commands");
+      }
+      if (game.x >430 && game.y>250){
+        game.endGame();
+        System.out.println("score=" + score);
+        System.out.println("steps=" + game.getSteps());
+      }
+  }
 
-    score += game.endGame();
-
-    System.out.println("score=" + score);
-    System.out.println("steps=" + game.getSteps());
+    
   }
 }
 
